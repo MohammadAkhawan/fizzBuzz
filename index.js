bf35e825e6fc67d8881cda16ba891995c59dfb33
mammad <113013285+MohammadAkhawan@users.noreply.github.com>
2024-02-23 22:16:13 -0500

    Finish startup page

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-let userInput = 50;
+let userInput;
 let counter = 1;
 let finalString = "0 -";
 
-const divisibleToThree = num => num % 3 === 0;
-const divisibleToFive = num => num % 5 === 0;
-const divisibleToThreeAndFive = num => (num % 3 === 0 && num % 5 === 0);
+const divisibleToThree = (num) => num % 3 === 0;
+const divisibleToFive = (num) => num % 5 === 0;
+const divisibleToThreeAndFive = (num) => num % 3 === 0 && num % 5 === 0;
 
-while(userInput > 0){
-    switch (true){
-        case (divisibleToThreeAndFive(counter)):
+while (userInput > 0) {
+    switch (true) {
+        case divisibleToThreeAndFive(counter):
             finalString += " FizzBuzz -";
             userInput--;
             counter++;
             break;
-        case (divisibleToThree(counter)):
+        case divisibleToThree(counter):
             finalString += " Fizz -";
             userInput--;
             counter++;
             break;
-        case (divisibleToFive(counter)):
+        case divisibleToFive(counter):
             finalString += " Buzz -";
             userInput--;
             counter++;
@@ -31,5 +31,4 @@ while(userInput > 0){
     }
 }
 
-console.log(userInput);
 console.log(finalString);
